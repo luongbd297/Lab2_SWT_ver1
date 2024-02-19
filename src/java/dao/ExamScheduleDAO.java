a907dac1feba473bd6d0de7ba7b369a973896042
luongbd297 <155948958+luongbd297@users.noreply.github.com>
2024-02-20 06:03:02 +0700

    Update ExamScheduleDAO.java

--- src/java/dao/ExamScheduleDAO.java
+++ src/java/dao/ExamScheduleDAO.java
@@ -17,25 +17,43 @@ public class ExamScheduleDAO extends dal.DBContext {
         String query = "SELECT * FROM  ExamSchedules INNER JOIN Subjects \n"
                 + "ON ExamSchedules.SubjectID = Subjects.SubjectID INNER JOIN \n"
                 + "Locations ON ExamSchedules.LocationID = Locations.LocationID";
+        PreparedStatement stmt = null;
+    ResultSet rs = null;
 
-        try {
-            PreparedStatement stmt = connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                String examId = String.valueOf(rs.getInt("ExamId"));
-                String subject = rs.getString("SubjectName");
-                String examDate = rs.getString("ExamDate");
-                String location = rs.getString("Room") + ", " + rs.getString("SiteName") + ", " + rs.getString("ClusterName");
-                String startTime = rs.getString("StartAt");
-                String endTime = rs.getString("EndAt");
+    try {
+        stmt = connection.prepareStatement(query);
+        rs = stmt.executeQuery();
+    
+        while (rs.next()) {
+            String examId = String.valueOf(rs.getInt("ExamId"));
+            String subject = rs.getString("SubjectName");
+            String examDate = rs.getString("ExamDate");
+            String location = rs.getString("Room") + ", " + rs.getString("SiteName") + ", " + rs.getString("ClusterName");
+            String startTime = rs.getString("StartAt");
+            String endTime = rs.getString("EndAt");
 
-                ExamSchedule examSchedule = new ExamSchedule(examId, subject, examDate, location, startTime, endTime);
-                examSchedules.add(examSchedule);
-            }
-
-        } catch (SQLException ex) {
-            ex.getMessage();
+            ExamSchedule examSchedule = new ExamSchedule(examId, subject, examDate, location, startTime, endTime);
+            examSchedules.add(examSchedule);
         }
+    } catch (SQLException ex) {
+        ex.getMessage();
+    } finally {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException ex) {
+                ex.getMessage();
+            }
+        }
+    
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+                ex.getMessage();
+            }
+        }
+    }
 
         return examSchedules;
     }
